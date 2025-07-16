149bedcea2279a0c60204cb3ec3571e15a874148
Intesar Shawon <intesarshawon@gmail.com>
2025-07-17 03:57:43 +0600

    detect geolocation and reroute

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import LocationDetector from "@/components/LocationDetector";
 
 export default function Home() {
+
   return (
     <>
       <div>
-        hp
+        <LocationDetector/>
       </div>
     </>
   );
